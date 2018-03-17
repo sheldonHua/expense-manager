d3f37f12402412fc6b7f167fa6dbf01199861140
sheldon <sheldon.hua@koodomobile.com>
2018-03-17 13:57:58 -0400

    Add todo list components to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header'
+import ExpenseForm from './components/ExpenseForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Expense Manager</h1>
+        <Header title="Expense Manager" />
+        <ExpenseForm />
       </div>
     );
   }
